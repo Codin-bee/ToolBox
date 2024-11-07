789315b9e1b345d069d5d5a3d836947a5a448ce9
theco <thecodingbee.dev@gmail.com>
2024-11-07 20:05:08 +0100

    Updated the Algorithms.getIndexWithHighestVal method and added support for int, float, long and short data-types

--- src/main/java/com/codingbee/tool_box/algorithms/Algorithms.java
+++ src/main/java/com/codingbee/tool_box/algorithms/Algorithms.java
@@ -4,6 +4,7 @@ import com.codingbee.tool_box.exceptions.IncorrectDataException;
 
 @SuppressWarnings("unused")
 public class Algorithms {
+    //Double
     /**
      * Simple method used to find the element with the highest value.
      * Special case: If the highest value is found at more indexes returns the lowest of the indexes.
@@ -17,6 +18,98 @@ public class Algorithms {
         }
         int indexWithHighestNo = 0;
         double highestNo = Double.MIN_VALUE;
+        for (int i = 0; i < values.length; i++) {
+            if (values[i]>highestNo){
+                highestNo = values[i];
+                indexWithHighestNo = i;
+            }
+        }
+        return indexWithHighestNo;
+    }
+
+    //Int
+    /**
+     * Simple method used to find the element with the highest value.
+     * Special case: If the highest value is found at more indexes returns the lowest of the indexes.
+     * @param values array where the algorithm searches.
+     * @return index of the element with the highest value
+     * @throws IncorrectDataException in case the inserted array is null
+     */
+    public static int getIndexWithHighestVal(int[] values) throws IncorrectDataException {
+        if(values == null){
+            throw new IncorrectDataException("Get index with highest value - the array must not be null");
+        }
+        int indexWithHighestNo = 0;
+        int highestNo = values[0];
+        for (int i = 0; i < values.length; i++) {
+            if (values[i]>highestNo){
+                highestNo = values[i];
+                indexWithHighestNo = i;
+            }
+        }
+        return indexWithHighestNo;
+    }
+
+    //Float
+    /**
+     * Simple method used to find the element with the highest value.
+     * Special case: If the highest value is found at more indexes returns the lowest of the indexes.
+     * @param values array where the algorithm searches.
+     * @return index of the element with the highest value
+     * @throws IncorrectDataException in case the inserted array is null
+     */
+    public static int getIndexWithHighestVal(float[] values) throws IncorrectDataException {
+        if(values == null){
+            throw new IncorrectDataException("Get index with highest value - the array must not be null");
+        }
+        int indexWithHighestNo = 0;
+        float highestNo = values[0];
+        for (int i = 0; i < values.length; i++) {
+            if (values[i]>highestNo){
+                highestNo = values[i];
+                indexWithHighestNo = i;
+            }
+        }
+        return indexWithHighestNo;
+    }
+
+    //Long
+    /**
+     * Simple method used to find the element with the highest value.
+     * Special case: If the highest value is found at more indexes returns the lowest of the indexes.
+     * @param values array where the algorithm searches.
+     * @return index of the element with the highest value
+     * @throws IncorrectDataException in case the inserted array is null
+     */
+    public static int getIndexWithHighestVal(long[] values) throws IncorrectDataException {
+        if(values == null){
+            throw new IncorrectDataException("Get index with highest value - the array must not be null");
+        }
+        int indexWithHighestNo = 0;
+        long highestNo = values[0];
+        for (int i = 0; i < values.length; i++) {
+            if (values[i]>highestNo){
+                highestNo = values[i];
+                indexWithHighestNo = i;
+            }
+        }
+        return indexWithHighestNo;
+    }
+
+    //Short
+    /**
+     * Simple method used to find the element with the highest value.
+     * Special case: If the highest value is found at more indexes returns the lowest of the indexes.
+     * @param values array where the algorithm searches.
+     * @return index of the element with the highest value
+     * @throws IncorrectDataException in case the inserted array is null
+     */
+    public static int getIndexWithHighestVal(short[] values) throws IncorrectDataException {
+        if(values == null){
+            throw new IncorrectDataException("Get index with highest value - the array must not be null");
+        }
+        int indexWithHighestNo = 0;
+        short highestNo = values[0];
         for (int i = 0; i < values.length; i++) {
             if (values[i]>highestNo){
                 highestNo = values[i];

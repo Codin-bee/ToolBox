4daa7ced053a7d8d02431d4b0179662ae5079d29
theco <thecodingbee.dev@gmail.com>
2024-11-17 12:05:08 +0100

    Implemented Vectors methods for int data-type and fixed incorrect method call that wasn't fixed after some methods were moved in previous commits. (One method tried to call Algorithms.sumElements, but it is now part of Vectors class) Separated the code for each data-type to regions using comments. All the Javadoc of the new methods is linked to the original double implementation.

--- src/main/java/com/codingbee/tool_box/math/vectors/Vectors.java
+++ src/main/java/com/codingbee/tool_box/math/vectors/Vectors.java
@@ -5,6 +5,7 @@ import com.codingbee.tool_box.exceptions.IncorrectDataException;
 
 @SuppressWarnings("unused")
 public class Vectors {
+    //region Double
     /**
      * Adds elements of two vector at each index and returns new vector containing th sums.
      * @param vectorA first vector
@@ -148,6 +149,135 @@ public class Vectors {
         if (array == null){
             throw new IncorrectDataException("The array can not be null pointer");
         }
-        return Algorithms.sumElements(array) / array.length;
+        return Vectors.sumElements(array) / array.length;
     }
+    //endregion Double
+
+    //region Int
+    /**
+     * This method works exactly as: {@link #add(double[], double[])}
+     */
+    public static int[] add(int[] vectorA, int[] vectorB){
+        if (vectorA == null || vectorB == null){
+            throw new IncorrectDataException("The arrays can not be a null pointers");
+        }
+        if (vectorA.length != vectorB.length){
+            throw new IncorrectDataException("The lengths of arras have to be the same in order to add them");
+        }
+        int[] vectorC = new int[vectorA.length];
+        for (int i = 0; i < vectorA.length; i++) {
+            vectorC[i] = vectorA[i] + vectorB[i];
+        }
+        return vectorC;
+    }
+
+    /**
+     * This method works exactly as: {@link #multiply(double[], double[])}
+     */
+    public static int[] multiply(int[] vectorA, int[] vectorB){
+        if (vectorA == null || vectorB == null){
+            throw new IncorrectDataException("The arrays can not be a null pointers");
+        }
+        if (vectorA.length != vectorB.length){
+            throw new IncorrectDataException("The lengths of the array have to be the same in order to multiply them");
+        }
+        int[] vectorC = new int[vectorA.length];
+        for (int i = 0; i < vectorA.length; i++) {
+            vectorC[i] = vectorA[i] * vectorB[i];
+        }
+        return vectorC;
+    }
+
+    /**
+     * This method works exactly as: {@link #subtract(double[], double[])}
+     */
+    public static int[] subtract(int[] vectorA, int[] vectorB){
+        if (vectorA == null || vectorB == null){
+            throw new IncorrectDataException("The arrays can not be a null pointers");
+        }
+        if (vectorA.length != vectorB.length){
+            throw new IncorrectDataException("The arrays need to have the same length in order to " +
+                    "subtract them from each other");
+        }
+        int[] vectorC = new int[vectorA.length];
+        for (int i = 0; i < vectorA.length; i++) {
+            vectorC[i] = vectorA[i] - vectorB[i];
+        }
+        return vectorC;
+    }
+
+    /**
+     * This method works exactly as: {@link #divide(double[], double[])}
+     */
+    public static int[] divide(int[] vectorA, int[] vectorB){
+        if (vectorA == null || vectorB == null){
+            throw new IncorrectDataException("The arrays can not be a null pointers");
+        }
+        if (vectorA.length != vectorB.length){
+            throw new IncorrectDataException("The arrays need to have the same length in order to " +
+                    "divide the first one by the second one");
+        }
+        int[] vectorC = new int[vectorA.length];
+        for (int i = 0; i < vectorA.length; i++) {
+            vectorC[i] = vectorA[i] / vectorB[i];
+        }
+        return vectorC;
+    }
+
+    /**
+     * This method works exactly as: {@link #sumElements(double[])}
+     */
+    public static int sumElements(int[] array){
+        if (array == null){
+            throw new IncorrectDataException("The array can not be null pointer");
+        }
+        int sum = 0;
+        for (int element : array) {
+            sum += element;
+        }
+        return sum;
+    }
+
+    /**
+     * This method works exactly as: {@link #findMax(double[])}
+     */
+    public static int findMax(int[] array){
+        if (array == null){
+            throw new IncorrectDataException("The array can not be null pointer");
+        }
+        int max = array[0];
+        for (int element : array) {
+            if (element > max) {
+                max = element;
+            }
+        }
+        return max;
+    }
+
+    /**
+     * This method works exactly as: {@link #findMin(double[])}
+     */
+    public static int findMin(int[] array){
+        if (array == null){
+            throw new IncorrectDataException("The array can not be null pointer");
+        }
+        int min = array[0];
+        for (int element : array) {
+            if (element < min) {
+                min = element;
+            }
+        }
+        return min;
+    }
+
+    /**
+     * This method works exactly as: {@link #calculateAverage(double[])}
+     */
+    public static int calculateAverage(int[] array){
+        if (array == null){
+            throw new IncorrectDataException("The array can not be null pointer");
+        }
+        return Vectors.sumElements(array) / array.length;
+    }
+    //endregion Int
 }
